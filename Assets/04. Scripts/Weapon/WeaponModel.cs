e9c381c651763696f2c7139212f4d232c5dec9cc
SoKH-creator <skh7235@naver.com>
2025-09-05 20:44:24 +0900

    fix/무기 원본 데이터를 수정하던 오류 수정

--- Assets/04. Scripts/Weapon/WeaponModel.cs
+++ Assets/04. Scripts/Weapon/WeaponModel.cs
@@ -7,9 +7,9 @@ public class WeaponModel
 {
     private WeaponData _data;
     private WeaponState _state;
-    
-    public WeaponData Data { get => _data; }
-    public WeaponState State { get => _state; set => _state = value; }
+
+    public WeaponData Data { get { return _data; } }
+    public WeaponState State { get { return _state; } set { _state = value; } }
 
     public event Action OnChanged;
 
@@ -21,57 +21,31 @@ public class WeaponModel
 
     public bool TryUnlock(ref int exp)
     {
-        if (exp >= _data.requiredExp)
-        {
-            exp -= _data.requiredExp;
-            _data.requiredExp += _data.deltaRequiredExp;
-            _state.unlocked = true;
-            OnChanged?.Invoke();
-            return true;
-        }
-        else { return false; }            
+        int cost = GetRequiredExp();
+        if (_state.unlocked || exp < cost)
+            return false;
+        
+        exp -= cost;
+        _state.unlocked = true;
+        OnChanged?.Invoke();
+        return true;
     }
     public bool TryUpgrade(ref int exp)
     {
-        if (exp >= _data.requiredExp)
-        {
-            exp -= _data.requiredExp;
-
-            _data.baseAttack += _data.deltaAttack;
-            _data.baseCritRate += _data.deltaCritRate;
-            _data.requiredExp += _data.deltaRequiredExp;
-            OnChanged?.Invoke();
-            return true;
-        }
-        else { return false; }
-    }
-
-    public float GetAttack()
-    {
-        float atk = _data.baseAttack;
+        if (!_state.unlocked) return false;
         
-        for (int i = 0; i< _state.level; i++)
-            atk += _data.deltaAttack;
+        int cost = GetRequiredExp();
+        if (exp < cost)
+            return false;
 
-        return atk;
+        exp -= cost;
+        _state.level += 1;
+        OnChanged?.Invoke();
+        return true;
     }
-    public float GetCrit()
-    {
-        float crit = _data.baseCritRate;
-        
-        for (int i = 0; i < _state.level; i++)
-            crit += _data.deltaCritRate;
 
-        return crit;
-    }
-    public int GetRequiredExp()
-    {
-        int exp = _data.requiredExp;
-
-        for (int i = 0; i < _state.level; i++)
-            exp += _data.deltaRequiredExp;
-        
-        return exp;
-    }
+    public float GetAttack() => _data.baseAttack + _state.level * _data.deltaAttack;
+    public float GetCrit() => _data.baseCritRate + _state.level * _data.deltaCritRate;
+    public int GetRequiredExp() => _data.requiredExp + _state.level * _data.deltaRequiredExp;
 }
 
