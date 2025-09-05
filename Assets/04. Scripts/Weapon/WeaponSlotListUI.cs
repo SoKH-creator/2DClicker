3b90dac90bcaa1de9411f18c00bfc69875a0f48d
SoKH-creator <skh7235@naver.com>
2025-09-05 12:17:38 +0900

    chore/무기 가방 UI 프리팹화

--- Assets/04. Scripts/Weapon/WeaponSlotListUI.cs
+++ Assets/04. Scripts/Weapon/WeaponSlotListUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponBagUI : MonoBehaviour
+public class WeaponSlotListUI : MonoBehaviour
 {
     public GameObject slotPrefab;
     public Transform parentTransform;
